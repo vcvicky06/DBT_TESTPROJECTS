64d21b7706d6f0173e3df2d8b5fee3288f90e8df
vcv vignesh <vcvigneshwar06@gmail.com>
2026-05-29 04:41:56 +0000

    pre hook and post hook

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,19 +1,35 @@
 
 /*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
+Welcome to your first dbt model!
+Did you know that you can also configure models directly within SQL files?
+This will override configurations stated in dbt_project.yml
 
-    Try changing "table" to "view" below
+Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+--{{ config(materialized='table') }}
+
+{{
+
+    config(
+
+        materialized='table',
+
+        pre_hook="create table if not exists test_log (id int)",
+
+        post_hook="insert into test_log values (1)"
+
+    )
+
+}}
+
+
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select 2 as id
+select 1 as id
+union all
+select 2 as id
 
 )
 
@@ -21,7 +37,7 @@ select *
 from source_data
 
 /*
-    Uncomment the line below to remove records with null `id` values
+Uncomment the line below to remove records with null `id` values
 */
 
 -- where id is not null
